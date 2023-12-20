d885204daf2ac2ea11c3a8d1525cdf16b56a80e9
hutao <mm_Huang_work@163.com>
2023-12-21 00:16:42 +0800

    feat: 更新40页面

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<div className="w-full h-screen flex flex-1 justify-center items-center divide-x-2">
+			<h1 className="text-[32px] font-medium pr-4">404</h1>
+			<p className="pl-4">Page not found</p>
+		</div>
+	)
+}
